eee15522f22824324565ab7c7905f4d776b72baf
DrJKL <DrJKL0424@gmail.com>
2023-11-26 15:27:17 -0800

    Maybe register extension...

--- dist/comfyui-anchors.js
+++ dist/comfyui-anchors.js
@@ -1,6 +1,16 @@
-function o() {
+import { app as o } from "../../../scripts/app.js";
+function n() {
   console.log("Setting up ComfyUI-Anchors...");
 }
-export {
-  o as setupAnchors
-};
+o.registerExtension({
+  name: "drjkl.customnodes.anchors",
+  async beforeRegisterNodeDef(e, s, t) {
+    console.log(`cui-anchors_beforeRegisterNodeDef: ${e.type}`);
+  },
+  async nodeCreated(e) {
+    console.log(`cui-anchors_nodeCreated: ${e.getTitle()}`);
+  },
+  async setup() {
+    n();
+  }
+});
--- lib/anchor.ts
+++ lib/anchor.ts
@@ -1,3 +1,21 @@
-export function setupAnchors() {
+// @ts-ignore
+import { app } from '../../../scripts/app.js';
+
+import type { ComfyExtension } from './../types/comfy.d';
+
+function setupAnchors() {
   console.log(`Setting up ComfyUI-Anchors...`);
 }
+
+app.registerExtension({
+  name: 'drjkl.customnodes.anchors',
+  async beforeRegisterNodeDef(_nodeType, _nodeData, _app) {
+    console.log(`cui-anchors_beforeRegisterNodeDef: ${_nodeType.type}`);
+  },
+  async nodeCreated(_node) {
+    console.log(`cui-anchors_nodeCreated: ${_node.getTitle()}`);
+  },
+  async setup() {
+    setupAnchors();
+  },
+} satisfies Partial<ComfyExtension>);
