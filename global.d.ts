28cbb315320236d2929237c9be3623472a26f037
DrJKL <DrJKL0424@gmail.com>
2023-11-27 18:53:48 -0800

    The `any`s make me feel so dirty.

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-import {
-  LiteGraph as LiteGraphType,
-  LGraphCanvas as LGraphCanvasType,
-} from '../ComfyUI/web/lib/litegraph.core';
-// TODO: Oh god... What have I gotten myself into...?
-// Nah, this'll be fun.
-declare const LiteGraph: typeof LiteGraphType;
-declare const LGraphCanvas: typeof LGraphCanvasType;
---
+++ lib/global.d.ts
@@ -0,0 +1,4 @@
+// TODO: Oh god... What have I gotten myself into...?
+// Nah, this'll be fun.
+declare const LiteGraph: any;
+declare const LGraphCanvas: any;
